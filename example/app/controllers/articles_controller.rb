5405ef0e4177412248416c84e2315f0f6556f143
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-05-12 23:24:43 +0200

    make request a little bit slower to see loader :-)

--- example/app/controllers/articles_controller.rb
+++ example/app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all.paginate(:per_page => 10, :page => params[:page])
     if request.xhr?
+      sleep(3) # make request a little bit slower to see loader :-)
       render(:partial => "article", :collection => @articles)
     end
   end
