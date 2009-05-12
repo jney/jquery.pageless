90213b1168ef23718e7dd8989e593f7e3a2d5c6d
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-05-13 00:13:19 +0200

    smaller render

--- example/app/controllers/articles_controller.rb
+++ example/app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all.paginate(:per_page => 10, :page => params[:page])
     if request.xhr?
       sleep(3) # make request a little bit slower to see loader :-)
-      render(:partial => "article", :collection => @articles)
+      render :partial => @articles
     end
   end
 end
