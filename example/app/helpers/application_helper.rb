71993b8763e2109497a738bcdc05696248a42e9a
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-05-12 23:26:02 +0200

    helper really much cleaner using to_json method

--- example/app/helpers/application_helper.rb
+++ example/app/helpers/application_helper.rb
@@ -1,13 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def pageless(total_pages, url=nil)
-    javascript_tag(
-      "$('#results').pageless({" \
-        "totalPages:#{total_pages}," \
-        "url:'#{url}'," \
-        "params:{without_layout: true}, " \
-        "loaderMsg:'Loading more results'" \
-      "});"
-    )
+    opts = {
+      :totalPages => total_pages,
+      :url        => url,
+      :loaderMsg  => 'Loading more results'
+    }
+    
+    javascript_tag("$('#results').pageless(#{opts.to_json});")
   end
 end
