f90e6ae0fa8896c9e2eb4b93cc97757c3ab580bc
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2009-05-12 23:24:18 +0200

    using request.xhr? instead of parameter

--- example/app/controllers/articles_controller.rb
+++ example/app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   # GET /articles.xml
   def index
     @articles = Article.all.paginate(:per_page => 10, :page => params[:page])
-    if params[:without_layout]
+    if request.xhr?
       render(:partial => "article", :collection => @articles)
     end
   end
