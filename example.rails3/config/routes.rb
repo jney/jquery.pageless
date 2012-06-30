82d93e646be2975bf4c421337f2df99221d27d5b
Michael <michael@luckeneder.net>
2012-06-30 15:18:07 +0200

    modified root route

--- example.rails3/config/routes.rb
+++ example.rails3/config/routes.rb
@@ -1,5 +1,7 @@
 ExampleRails3::Application.routes.draw do
   resources :articles
+  
+  root :to=>"articles#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
